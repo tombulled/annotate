44e67cf1cd039fb9e09d417f716c4b54bce48a9b
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2022-02-08 15:14:38 +0000

    Remove dev code comments

--- annotate/__init__.py
+++ annotate/__init__.py
@@ -3,12 +3,6 @@ from .attribute import annotations
 from .models import Annotation
 from .decorators import annotation, marker
 
-# NOTE(s):
-#   Plumb in an event framework? e.g. @on(events.Annotate), @on(events.InheritAnnotations)
-#   Should `inherited` apply to methods?
-#   Use a flag for `targets`(e.g. None/False) to indicate *all* targets
-#   Python 3.8 or 3.9?
-
 get_raw_annotations = annotations.get
 has_annotations = annotations.has
 set_annotations = annotations.set
