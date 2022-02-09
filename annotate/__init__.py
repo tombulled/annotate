fbf4592d85dacf1314f02fc696eb0d8d29293434
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2022-02-09 20:47:19 +0000

    cd workflow: only commit formatting changes if present (fix typo))

--- annotate/__init__.py
+++ annotate/__init__.py
@@ -4,6 +4,7 @@ from .models import Annotation
 from .decorators import annotation, marker
 
 
+
 get_raw_annotations = annotations.get
 has_annotations = annotations.has
 set_annotations = annotations.set
