1aaba147bd7d4d9be3683a6600ca76577847c9af
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2022-02-10 21:37:47 +0000

    Tidy up CD workflow

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,25 +3,28 @@ import types
 import typing
 import annotate
 
+
 @pytest.fixture
 def func() -> types.FunctionType:
-    def foo(): ...
+    def foo():
+        ...
 
     return foo
 
+
 @pytest.fixture
 def cls() -> type:
-    class Foo: ...
+    class Foo:
+        ...
 
     return Foo
 
+
 @pytest.fixture
 def annotation() -> annotate.Annotation:
-    return annotate.Annotation('key', 'value')
+    return annotate.Annotation("key", "value")
+
 
 @pytest.fixture
 def annotations() -> typing.List[annotate.Annotation]:
-    return [
-        annotate.Annotation(f'key-{index}', f'value-{index}')
-        for index in range(3)
-    ]
+    return [annotate.Annotation(f"key-{index}", f"value-{index}") for index in range(3)]
