60332fee1b7f4890131e321b1f2dceee8b60a8dc
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2022-02-10 21:15:21 +0000

    ✨ Add Tests (#11)

---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+import pytest
+import types
+import typing
+import annotate
+
+@pytest.fixture
+def func() -> types.FunctionType:
+    def foo(): ...
+
+    return foo
+
+@pytest.fixture
+def cls() -> type:
+    class Foo: ...
+
+    return Foo
+
+@pytest.fixture
+def annotation() -> annotate.Annotation:
+    return annotate.Annotation('key', 'value')
+
+@pytest.fixture
+def annotations() -> typing.List[annotate.Annotation]:
+    return [
+        annotate.Annotation(f'key-{index}', f'value-{index}')
+        for index in range(3)
+    ]
